932bfb02aa4cd74251c24cb305ba0941d917cf63
Sanchit Arora <sanchit827@gmail.com>
2024-10-25 12:01:32 -0400

    added logout

--- app/src/main/java/com/group18/androidproject1/ui/screens/menu/MenuActivity.kt
+++ app/src/main/java/com/group18/androidproject1/ui/screens/menu/MenuActivity.kt
@@ -9,34 +9,44 @@ import com.group18.androidproject1.ui.screens.about.AboutActivity
 import com.group18.androidproject1.ui.screens.animes.AnimesActivity
 import com.group18.androidproject1.ui.screens.manga.MangaActivity
 import com.group18.androidproject1.ui.screens.reviews.ReviewsActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.group18.androidproject1.MainActivity
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        auth = FirebaseAuth.getInstance()
 
         val btnTopAnimes = findViewById<Button>(R.id.btnAnimes)
         val btnTopManga = findViewById<Button>(R.id.btnManga)
         val btnReviews = findViewById<Button>(R.id.btnReviews)
         val btnAbout = findViewById<Button>(R.id.btnAbout)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
 
         btnTopAnimes.setOnClickListener {
-            val intent = Intent(this, AnimesActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, AnimesActivity::class.java))
         }
 
         btnTopManga.setOnClickListener {
-            val intent = Intent(this, MangaActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, MangaActivity::class.java))
         }
 
         btnReviews.setOnClickListener {
-            val intent = Intent(this, ReviewsActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, ReviewsActivity::class.java))
         }
+
         btnAbout.setOnClickListener {
-            val intent = Intent(this, AboutActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, AboutActivity::class.java))
+        }
+
+        btnLogout.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
