3bd175cc71fdac2124467a2c770eea9071da92f3
Deepak John <deepakjohn141@gmail.com>
2024-10-25 20:45:17 -0400

    updated MenuActivity

--- app/src/main/java/com/group18/androidproject1/ui/screens/menu/MenuActivity.kt
+++ app/src/main/java/com/group18/androidproject1/ui/screens/menu/MenuActivity.kt
@@ -2,39 +2,36 @@ package com.group18.androidproject1.ui.screens.menu
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.group18.androidproject1.R
+import com.group18.androidproject1.databinding.ActivityMenuBinding
 import com.group18.androidproject1.ui.screens.about.AboutActivity
 import com.group18.androidproject1.ui.screens.animes.AnimesActivity
 import com.group18.androidproject1.ui.screens.manga.MangaActivity
 import com.group18.androidproject1.ui.screens.reviews.ReviewsActivity
 
 class MenuActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnTopAnimes = findViewById<Button>(R.id.btnAnimes)
-        val btnTopManga = findViewById<Button>(R.id.btnManga)
-        val btnReviews = findViewById<Button>(R.id.btnReviews)
-        val btnAbout = findViewById<Button>(R.id.btnAbout)
 
-        btnTopAnimes.setOnClickListener {
+        binding.btnAnimes.setOnClickListener {
             val intent = Intent(this, AnimesActivity::class.java)
             startActivity(intent)
         }
 
-        btnTopManga.setOnClickListener {
+        binding.btnManga.setOnClickListener {
             val intent = Intent(this, MangaActivity::class.java)
             startActivity(intent)
         }
 
-        btnReviews.setOnClickListener {
+        binding.btnReviews.setOnClickListener {
             val intent = Intent(this, ReviewsActivity::class.java)
             startActivity(intent)
         }
-        btnAbout.setOnClickListener {
+        binding.btnAbout.setOnClickListener {
             val intent = Intent(this, AboutActivity::class.java)
             startActivity(intent)
         }
